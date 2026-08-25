67f6960b0a43ca5eecbdd149865e1197e4f2a39a
Shiwani543 <164074663+Shiwani543@users.noreply.github.com>
2026-08-25 14:06:37 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (73.21%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,19 +1,16 @@
-class Solution {
-    public int climbStairs(int n) {
-        if (n <= 2) {
-            return n;
+class Solution{
+    public int climbStairs(int n){
+        int[]memo = new int[n+1];
+        return helper(n, memo);
+    }
+    private int helper(int n,int[]memo){
+        if(n==0||n==1){
+            return 1;
         }
-
-        int prev2 = 1; 
-        int prev1 = 2; 
-
-        for (int i = 3; i <= n; i++) {
-            int curr = prev1 + prev2;
-            prev2 = prev1;
-            prev1 = curr;
+        if(memo[n]!=0){
+            return memo[n];
         }
-
-        return prev1;
-        
+        memo[n]= helper(n-1,memo)+helper(n-2,memo);
+        return memo[n];
     }
 }
